7349b9f11d6b6cecf44a1e33b7f428497ba8da32
HAERANG <abcrang@gmail.com>
2022-08-10 04:11:48 +0900

    3.4

--- src/index.js
+++ src/index.js
@@ -2,17 +2,33 @@
 import express from "express";
 // const express = require("express");
 
-const PORT = 4000;
-
 const app = express();
 
-/////////////////////////////// request, response개념 ////////////////////////////////
-// 브라우저가 get request하면("/"url이오면) 함수를 실행
-const handleHome = () => console.log("Somebody is trying to go home.");
+
+
+/////////////////////// request, response개념(~3.3강) //////////////////////////
+// 브라우저가 get request하면("/", 해당 url이오면) 함수를 실행
+
+const handleHome = (request, response) => {
+//     const btn = document.createElement("button");
+//     btn.innerText = "login here!";
+//     btn.style = "margin: 0px 10px 3px 0px";
+//     document.querySelector('body').appendChild(btn);
+    // return response.end(); //end()로 반응없이 끄기
+    return response.send("<h1>안녕하세용!</h1>")
+};
+
+const handleLogin = (potato, tomato) => {
+    return tomato.send({ messege:"Login here."}); // json으로 전송
+};
+
 
 app.get("/", handleHome);
+app.get("/login", handleLogin);
 
-const handleListening = () =>
-  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+
+/////////////////////////////// 서버오픈(~3.1강)////////////////////////////////
+const PORT = 4000;
+const handleListening = () => console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
