e87347625c0e30aa2beb17a7a1a6989df59ed5ba
HAERANG <abcrang@gmail.com>
2022-09-12 03:28:47 +0900

    edited env_db

--- src/db.js
+++ src/db.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.COOKIE_SECRET);
-// {   오류가 날경우 connect("주소", {요기})에 넣자
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//         useFindAndModify: false,
-//         useCreateIndex: true,
-// }
+mongoose.connect(process.env.DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+});
 
 const db = mongoose.connection;
 
